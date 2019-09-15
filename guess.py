66686e400d6fe740afa555cbd3727a0405854587
Peterson <contact.petersonac@gmail.com>
2019-09-15 19:39:03 -0300

    Final Version

--- guess.py
+++ guess.py
@@ -9,6 +9,10 @@ number_letters = int(input("How many letters your word have?\n"))
 using_alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 words = []
 
+have_letters = []
+word = ""
+multiple = False
+
 #load json file
 
 try:
@@ -22,19 +26,19 @@ except:
     
     sys.exit('No json file with {} letters'.format(number_letters))
 
-##check which letters will use to guess
-for a in using_alphabet:
-    has = False
-
-    for w in words:
-        if a in w:
-            has = True
-            break
-
-    if not(has):
-        using_alphabet.remove(a)
-
 ##functions to update the letters and words
+def updateAlpha():
+    for a in using_alphabet:
+        has = False
+
+        for w in words:
+            if a in w:
+                has = True
+                break
+
+        if not(has):
+            using_alphabet.remove(a)
+
 def dontHave( letter ):
     global words
     global using_alphabet
@@ -57,32 +61,44 @@ def have( letter ):
         if letter in w:
             new_words.append(w)
 
+    updateAlpha()
     words = new_words
+
+##main function
+def main():
+    global words
+
+    while(len(words) != 1):
+        for l in using_alphabet:
+            if numpy.array_equal(have_letters, using_alphabet) or len(using_alphabet) == 0:
+                if(len(words) > 0):
+                    global multiple
+                    global word
+
+                    multiple = True
+                    for k, w in enumerate(words):
+                        word = word + w + ('' if k == (len(words) - 1) else ' or ')
+                    return
+
+
+
+            if l in have_letters:
+                continue
+
+            choice = ""
+            while(not(choice.isalpha()) or not((choice.lower().startswith('y') or choice.lower().startswith('n')))):
+                choice = input("Your word has {}?".format(l))
+
+            if(choice.lower().startswith('y')):
+                have(l)
+                have_letters.append(l)
+            else:
+                dontHave(l)
+                break
+    word = words[0]
 
 #start
 
-have_letters = []
-word = ""
-
-while(len(words) != 1):
-    for l in using_alphabet:
-        if numpy.array_equal(have_letters, using_alphabet) or len(using_alphabet) == 0:
-            sys.exit('I couldn\'t guess your word :(')
-
-        if l in have_letters:
-            continue
-
-        choice = ""
-        while(not(choice.isalpha()) or not((choice.lower().startswith('y') or choice.lower().startswith('n')))):
-            choice = input("Your word has {}?".format(l))
-
-        if(choice.lower().startswith('y')):
-            have(l)
-            have_letters.append(l)
-        else:
-            dontHave(l)
-            break
-
-word = words[0]
+main()
 
 print('Your word is {}!'.format(word))
